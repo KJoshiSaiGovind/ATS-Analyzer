1a312297816ceeebbda81e8ceb31f37b39e2da37
Karri Joshi Sai Govind <karrisaigovind33@gmail.com>
2026-07-14 17:44:53 +0530

    Add defaults to config settings for deployment

--- app/core/config.py
+++ app/core/config.py
@@ -2,10 +2,10 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    APP_NAME: str
-    APP_VERSION: str
+    APP_NAME: str = "ATS Resume Screening"
+    APP_VERSION: str = "1.0.0"
 
-    DEBUG: bool
+    DEBUG: bool = False
 
     DB_HOST: str = "localhost"
     DB_PORT: int = 3306
@@ -15,10 +15,10 @@ class Settings(BaseSettings):
 
     DATABASE_URL: str | None = None
 
-    SECRET_KEY: str
-    ALGORITHM: str
+    SECRET_KEY: str = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+    ALGORITHM: str = "HS256"
 
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
     class Config:
         env_file = ".env"
